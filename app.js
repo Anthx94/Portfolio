10b5c3faa3ff5baa3f2f5e529735cb08fa0bd859
Anthony Goncalves <anthonygoncalves94@outlook.fr>
2022-09-28 17:31:54 +0200

    modification

--- app.js
+++ app.js
@@ -3,12 +3,12 @@
 // Light and Dark themes
 const switcher = document.querySelector('.btn');
 
-switcher.addEventListener('click', function() {
+switcher.addEventListener('click', function () {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
 
     const className = document.body.className;
-    if(className == "light-theme") {
+    if (className == "light-theme") {
         this.textContent = "Dark";
     } else {
         this.textContent = "Light";
@@ -16,3 +16,6 @@ switcher.addEventListener('click', function() {
 
     console.log('current class name: ' + className);
 });
+
+
+// Text Inde Block Animation
