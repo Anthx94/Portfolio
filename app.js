d2a785284640c884db376794ba9b2e65dceeb53f
Anthony Goncalves <anthonygoncalves94@outlook.fr>
2022-10-12 16:44:42 +0200

    Modifs JS

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
 // On page load set the theme.
 (function () {
     let onpageLoad = localStorage.getItem("theme");
-    let element = document.body;
-    element.classList.add(onpageLoad);
+    if (onpageLoad == ""){
 
-    document.getElementById("btn").innerHTML = "Light";
+    } else { 
+        document.body.classList.add(onpageLoad); 
+        document.getElementById("btn").innerHTML = "Light";
+    }
+
+    
 })();
 
 
+// Change mode view
 function themeToggle() {
-    let element = document.body;
-    element.classList.toggle("dark-mode");
+    document.body.classList.toggle("dark-mode");
     var name = document.querySelector('.btn');
 
     let theme = localStorage.getItem("theme");
     if (theme && theme === "dark-mode") {
         localStorage.setItem("theme", "");
-        localStorage.setItem("mode", "Light");
         name.innerHTML = "Dark";
     } else {
         localStorage.setItem("theme", "dark-mode");
-        localStorage.setItem("mode", "Dark");
         name.innerHTML = "Light";
     }
 }
